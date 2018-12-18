30cfad20653939cf11bbd0cd73c897ecf41d96da
ahmednasser <ahmednasser2337@gmail.com>
2018-12-19 00:36:53 +0200

    add Halt System Call and some utils

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -1,6 +1,7 @@
 #include "userprog/syscall.h"
 #include <stdio.h>
 #include <syscall-nr.h>
+#include <src/devices/shutdown.h>
 #include "threads/interrupt.h"
 #include "threads/thread.h"
 
@@ -15,6 +16,41 @@ syscall_init (void)
 static void
 syscall_handler (struct intr_frame *f UNUSED) 
 {
+  //MBY AHMED NASSER
+  //IMPLEMENT SWITCH TO CHOOSE SYSTEM CALL DEPEND ON CODES
+  // WE need To check Arguements and Mapping them using esp
+  //TODO HESHAM
+  switch ((int)f){
+    case  SYS_HALT : {
+      if(check_arguemnts()) {
+        Call_Halt();
+      }
+      break;
+    }
+    case  SYS_EXIT :{
+      if (check_arguemnts()) {
+        Call_EXIT();
+      }
+      break;
+    }
+    case  SYS_EXEC :{
+      if (check_arguemnts()) {
+        Call_EXEC();
+      }
+      break;
+    }
+  }
   printf ("system call!\n");
   thread_exit ();
+}
+void Call_Halt (){
+  shutdown_power_off();
+};
+
+void Call_EXIT (){
+};
+void Call_EXEC (){
+};
+boolean check_arguemnts (){
+
 }
--- src/userprog/syscall.h
+++ src/userprog/syscall.h
@@ -1,6 +1,13 @@
 #ifndef USERPROG_SYSCALL_H
 #define USERPROG_SYSCALL_H
 
+#include <jmorecfg.h>
+
 void syscall_init (void);
+void Call_Halt();
+void Call_EXIT();
+void Call_EXEC();
+boolean check_arguemnts(void);
+
 
 #endif /* userprog/syscall.h */
